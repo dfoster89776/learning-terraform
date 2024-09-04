99f19801461c8d1ba5e9b4262baebb464b1a02e6
David Foster <dfoster89776@icloud.com>
2024-09-04 09:26:16 +0100

    Create new dev vpc

--- main.tf
+++ main.tf
@@ -18,6 +18,21 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -27,6 +42,8 @@ resource "aws_instance" "blog" {
   }
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 }
 
 module "blog_sg" {
@@ -34,7 +51,7 @@ module "blog_sg" {
   version = "4.13.0"
   name = "blog"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
   
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
