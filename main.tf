1e3587fee6f6981b1226c583c5dda583fc7f80ed
David Foster <dfoster89776@icloud.com>
2024-09-04 09:57:15 +0100

    Added AutoScaling

--- main.tf
+++ main.tf
@@ -33,20 +33,24 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.0.0"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 3
+  
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  tags = {
-    Name = "HelloWorld"
-  }
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
