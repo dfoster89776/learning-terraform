7578b60cf8f0c57feb964475c23698b0cfad2de7
David Foster <dfoster89776@icloud.com>
2024-09-04 11:11:23 +0100

    Update

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ module "blog_alb" {
       port     = 80
       protocol = "HTTP"
       forward = {
-        target_group_key = "ex-instance"
+        target_group_arn = module.blog.arn
       }
     }
   }
